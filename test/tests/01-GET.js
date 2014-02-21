43d67046b5ba26f13b48fedc7c98c74aec928d8c
Alexander Ose <alex@alexose.com>
2014-02-21 13:22:44 -0800

    add basic test framework

---
+++ test/tests/01-GET.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name : "Simple GET",
+	json : {
+		"url" : "/"
+	},
+	expected : "GET Request"
+}
---
+++ test/tests/02-POST.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name : "Simple POST",
+	json : {
+		url : "/",
+		method : "POST"
+	},
+	expected : "POST Request"
+}
