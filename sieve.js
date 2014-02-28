441969aa88a6250e316395e2d99398a29e5e0c54
Alexander Ose <alex@alexose.com>
2014-02-28 07:31:08 -0800

    move select logic into its own file

--- sieve.js
+++ sieve.js
@@ -7,7 +7,8 @@ var http = require('http')
 // Local modules
 var template = require('./lib/template')
   , validate = require('./lib/validate')
-  , error = require('./lib/error');
+  , error = require('./lib/error')
+  , select = require('./lib/select');
 
 // External dependencies
 var cache = require('memory-cache');
@@ -277,53 +278,10 @@ Sieve.prototype.cache = {
   }
 }
 
-// Attempt to apply selector(s)
-Sieve.prototype.select = function(corpus, selector, name, cb){
-
-  if (typeof(selector) === "object"){
-    
-    var results = {}
-      , expected = (Object.keys(selector).length);
-
-    for (var key in selector){
-      if (selector.hasOwnProperty(key)){
-        select.call(this, corpus, selector[key], name, accumulate.bind(this, key));
-      }
-    }
-  
-
-    function accumulate(key, result){
-      results[key] = result;
-
-      if (Object.keys(results).length === expected){
-        cb(results);
-      }
-    }
-  } else {
-    select.call(this, corpus, selector, name, cb);
-  }
-
-  function select(corpus, selector, name, cb){
-    try {
-
-      // Assume jsonselect unless otherwise specified
-      var engine = require('./plugins/' + (name || 'jsonselect'));
-    } catch(e){
-      error(e);
-    }
-
-    try{
-      engine.call(this, corpus, selector, cb);
-    } catch(e){
-      error(e);
-    }
-  }
-}
-
 Sieve.prototype.accumulate = function (entry, result, pos){
 
   if (entry.selector){
-     this.select(result, entry.selector, entry.engine, selected.bind(this));
+    select(result, entry.selector, entry.engine, selected.bind(this));
   } else {
     selected.call(this, result);
   }
