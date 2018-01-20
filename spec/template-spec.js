93e43141965229f17333a3052fb1557d9c706f24
Alexander Ose <alex@alexose.com>
2018-01-19 19:36:28 -0800

    support data arrays

--- spec/template-spec.js
+++ spec/template-spec.js
@@ -16,4 +16,43 @@ describe('templating tests', function() {
     done();
   });
   
+  it('should support arrays', function(done){
+    var arr = ['one', 'two', 'three'],
+        data = { page: arr },
+        entry = { url: 'http://example.com/{{page}}' };
+        expected = [
+          { url: 'http://example.com/one' },
+          { url: 'http://example.com/two' },
+          { url: 'http://example.com/three' }
+        ];
+   
+    var result = template(entry, data);
+    assert.deepEqual(result, expected);
+    done();
+  });
+  
+  it('should support a mix of arrays and strings', function(done){
+    var arr = ['one', 'two', 'three'],
+        data = {
+          domain: 'example',
+          page: arr, 
+          subdirectory: arr 
+        },
+        entry = { url: 'http://{{domain}}.com/{{subdirectory}}/{{page}}' };
+        expected = [
+          { url: 'http://example.com/one/one' },
+          { url: 'http://example.com/one/two' },
+          { url: 'http://example.com/one/three' },
+          { url: 'http://example.com/two/one' },
+          { url: 'http://example.com/two/two' },
+          { url: 'http://example.com/two/three' },
+          { url: 'http://example.com/three/one' },
+          { url: 'http://example.com/three/two' },
+          { url: 'http://example.com/three/three' }
+        ];
+   
+    var result = template(entry, data);
+    assert.deepEqual(result, expected);
+    done();
+  });
 });
